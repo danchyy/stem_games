7c0f9b2f9cde31561f0ca8853c8a504e8bd25300
Daniel Bratulić <danielbratulic@yahoo.com>
2018-05-09 12:08:10 +0200

    Moved sleep

--- request_handler.py
+++ request_handler.py
@@ -1,5 +1,6 @@
 import requests
 from task_pillars.pillars import get_solution
+from time import sleep
 
 headers = {
     "Authorization" : "token 5Jx2jthg8uTMUyRK6FoRYagfvEow6wNo"
@@ -14,10 +15,8 @@ def get_method(problem_id):
     print(headers)
     response = requests.get(url=url, headers=headers)
     print("=========================")
-    print("POST RESPONSE")
+    print("GET RESPONSE")
     print(response.text)
-    if not "error" in response.json():
-        print(response.text)
     return response.json()
 
 def work_task_pillars(json_data):
@@ -40,18 +39,26 @@ def post_method(problem_id, submission_id, solution):
     print("url: " + url)
     print(headers)
     print(params)
-    response = requests.post(url=url, params=params, headers=headers)
+    error = True
+    while error:
+        response = requests.post(url=url, params=params, headers=headers)
+        error = "error" in response.json()
+
     print("=========================")
     print("POST RESPONSE")
     print(response.text)
 
 def loop(problem_id):
+    counter = 0
     while True:
+        sleep(0.1)
         json_data = get_method(problem_id)
         if "error" in json_data:
             continue
         output, submission_id = work_task_pillars(json_data)
         post_method(problem_id=problem_id, submission_id=submission_id, solution=output)
+        counter += 1
+        print("CURRENT LOOP: " + str(counter))
 
 if __name__ == '__main__':
     loop(problem_id="4292bf95-9793-48b5-9576-daa6d2685e20")
