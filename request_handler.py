4c88422bed8ae4c74aee87d65e874717efd7e6b9
Daniel Bratulić <danielbratulic@yahoo.com>
2018-05-09 16:06:05 +0200

    Changes

--- request_handler.py
+++ request_handler.py
@@ -10,21 +10,35 @@ headers = {
 
 sleep_period = 0.84375
 
+task_problem_id_dict = {
+    "pillars": "4292bf95-9793-48b5-9576-daa6d2685e20",
+    "bla": "4292bf95-9793-48b5-9576-daa6d2685e20"
+}
 
-def get_method(problem_id):
+
+def try_get(problem_id):
+    error = True
     url = "https://tech.stemgames.hr/api/competitive/v1/"
     url += problem_id
-    print("=========================")
-    print("GET REQUEST")
-    print("url: " + url)
-    print(headers)
-    response = requests.get(url=url, headers=headers)
-    print("=========================")
-    print("GET RESPONSE")
-    print("Response text: ", end="")
-    print(response.text)
-    print("=========================")
-    return response.json()
+    while error:
+        print("GET REQUEST")
+        print("url: " + url)
+        print(headers)
+        response = requests.get(url=url, headers=headers)
+        sleep(sleep_period)
+        print("=========================")
+        print("GET RESPONSE")
+        print("Response text: ", end="")
+        print(response.text)
+        json_data = {}
+        try:
+            json_data = response.json()
+        except Exception:
+            print("No json in response")
+            continue
+        error = "error" in json_data
+        print("=========================\n")
+    return json_data
 
 
 def work_task(json_data, get_solution_method):
@@ -50,28 +64,74 @@ def post_method(problem_id, submission_id, solution):
     error = True
     while error:
         response = requests.post(url=url, data=solution, headers=headers)
-        error = "error" in response.json()
+        json_data = {}
+        try:
+            json_data = response.json()
+        except Exception:
+            print("No json in response")
+            continue
+        error = "error" in json_data
         print("=========================")
         print("POST RESPONSE")
         print("Response text: ", end="")
         print(response.text)
-        global sleep_period
         sleep(sleep_period)
     print("=========================")
+    print("\n\n")
 
-def loop(problem_id, solution_method):
+
+def get_get_solution_method(task_name):
+    script_name = task_name
+    method_name = "get_solution"
+    task_package = "task_" + task_name
+
+    # Importing the module from string
+    pillars_module = importlib.import_module(task_package + "." + script_name)
+    # Retrieving the method from module
+    method = getattr(pillars_module, method_name)
+
+    return method
+
+
+def determine_solving_task():
+    """
+
+    :return: name of task with highest number of points
+    """
+    max_points, max_key = None, None
+    for key in task_problem_id_dict:
+        problem_id = task_problem_id_dict[key]
+        response_for_problem = try_get(problem_id=problem_id)
+        available_points = response_for_problem["points_if_correct"]
+        print("AVAILABLE POINTS: " + str(available_points) + " FOR KEY: " + key)
+        if max_points is None or available_points > max_points:
+            max_points = available_points
+            max_key = key
+
+    print("BEST TASK IS: " + max_key + " WITH " + str(max_points))
+    return max_key
+
+
+def get_problem_id_and_method():
+    best_task_name = determine_solving_task()
+    problem_id = task_problem_id_dict[best_task_name]
+    get_solution_method = get_get_solution_method(best_task_name)
+    return problem_id, get_solution_method
+
+
+def loop():
     counter = 0
+    problem_id, solution_method = get_problem_id_and_method()
     while True:
         start = time()
         counter += 1
         print("CURRENT LOOP: " + str(counter))
-        json_data = get_method(problem_id)
-        global sleep_period
-        sleep(sleep_period)
-        if "error" in json_data:
-            continue
+        if counter % 10 == 0:
+            problem_id, solution_method = get_problem_id_and_method()
+        json_data = try_get(problem_id)
         output, submission_id = work_task(json_data, get_solution_method=solution_method)
         post_method(problem_id=problem_id, submission_id=submission_id, solution=output)
+
         print("TIME: {}\n\n".format(time() - start))
 
 
@@ -99,4 +159,4 @@ if __name__ == '__main__':
     # Add api key to header
     headers["Authorization"] = "token " + args.api
 
-    loop(problem_id=args.problem, solution_method=method)
+    loop()
